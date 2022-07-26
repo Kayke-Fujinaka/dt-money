71a8285654a888cc05f749ed7d833840623828d4
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2022-07-26 09:05:12 -0300

    style: highlight-background in summary component

--- src/components/Summary/styles.ts
+++ src/components/Summary/styles.ts
@@ -25,5 +25,10 @@ export const Container = styled.div`
         font-weight: 500;
         line-height: 3rem;
     }
+
+    &.highlight-background {
+        background-color: var(--green);
+        color: var(--shape);
+    }
   }
 `;
