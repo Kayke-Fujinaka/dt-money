f5fa979e8626bb61b0b764eab58a38955d6443c9
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2022-07-26 08:35:53 -0300

    feat: create header component

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,10 @@
+import * as S from "./styles";
+
+export function Header() {
+  return (
+    <S.Container>
+      <S.Content>
+      </S.Content>
+    </S.Container>
+  );
+}
---
+++ src/components/Dashboard/styles.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Container = styled.header`
+  background: var(--blue);
+`;
+
+export const Content = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin: 8 auto;
+  padding: 2rem 2rem 12rem;
+`;
