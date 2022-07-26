bbff7712a1c56d552f4a9de67f5502d40e0db6c1
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2022-07-26 09:18:45 -0300

    feat: create transaction table

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -2,6 +2,38 @@ import * as S from "./styles"
 
 export function TransactionTable() {
     return (
-        <S.Container></S.Container>
+        <S.Container>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Título</th>
+                        <th>Valor</th>
+                        <th>Categoria</th>
+                        <th>Data</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr>
+                        <td>Desenvolvimento de Website</td>
+                        <td>R$12000,00</td>
+                        <td>Desenvolvimento</td>
+                        <td>26/07/2022</td>
+                    </tr>
+                    <tr>
+                        <td>Desenvolvimento de Website</td>
+                        <td>R$12000,00</td>
+                        <td>Desenvolvimento</td>
+                        <td>26/07/2022</td>
+                    </tr>
+                    <tr>
+                        <td>Desenvolvimento de Website</td>
+                        <td>R$12000,00</td>
+                        <td>Desenvolvimento</td>
+                        <td>26/07/2022</td>
+                    </tr>
+                </tbody>
+            </table>
+        </S.Container>
     )
 }
