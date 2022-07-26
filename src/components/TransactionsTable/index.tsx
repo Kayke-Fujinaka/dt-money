dd666e96e3b88a4b98722c538ce218f3f37cd9e3
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2022-07-26 11:38:41 -0300

    feat: consume api with miragejs

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,33 +1,39 @@
-import * as S from "./styles"
+import { useEffect } from "react";
+import { api } from "../../services/api";
+import * as S from "./styles";
 
 export function TransactionTable() {
-    return (
-        <S.Container>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Título</th>
-                        <th>Valor</th>
-                        <th>Categoria</th>
-                        <th>Data</th>
-                    </tr>
-                </thead>
+  useEffect(() => {
+    api.get("transactions").then((response) => console.log(response.data));
+  }, []);
 
-                <tbody>
-                    <tr>
-                        <td>Desenvolvimento de Website</td>
-                        <td className="deposit">R$1.000,00</td>
-                        <td>Desenvolvimento</td>
-                        <td>26/07/2022</td>
-                    </tr>
-                    <tr>
-                        <td>Aluguel</td>
-                        <td className="withdraw">R$500,00</td>
-                        <td>Casa</td>
-                        <td>12/06/2022</td>
-                    </tr>
-                </tbody>
-            </table>
-        </S.Container>
-    )
+  return (
+    <S.Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Desenvolvimento de Website</td>
+            <td className="deposit">R$1.000,00</td>
+            <td>Desenvolvimento</td>
+            <td>26/07/2022</td>
+          </tr>
+          <tr>
+            <td>Aluguel</td>
+            <td className="withdraw">R$500,00</td>
+            <td>Casa</td>
+            <td>12/06/2022</td>
+          </tr>
+        </tbody>
+      </table>
+    </S.Container>
+  );
 }
