4bdd46b645430fd853bafbdc3fd3a238b85c64d0
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2022-07-26 11:54:29 -0300

    feat: start modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,29 @@
-import logoImg from "../../assets/logo.svg"
-import * as S from "./styles"
+import { useState } from "react";
+import Modal from 'react-modal';
+import logoImg from "../../assets/logo.svg";
+import * as S from "./styles";
 
 export function Header() {
-    return (
-        <S.Container>
-            <S.Content>
-            <img src={logoImg} alt="icon dt money" />
-            <button type="button">Nova Transação</button>
-            </S.Content>
-        </S.Container>
-    )
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true)
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false) 
+  }
+
+  return (
+    <S.Container>
+      <S.Content>
+        <img src={logoImg} alt="icon dt money" />
+        <button type="button" onClick={handleOpenNewTransactionModal}>Nova Transação</button>
+
+        <Modal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal}>
+          <h2>Cadastrar Transação</h2>
+        </Modal>
+      </S.Content>
+    </S.Container>
+  );
 }
